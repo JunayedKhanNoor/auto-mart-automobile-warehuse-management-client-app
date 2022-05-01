b492327546434e9ff94cd7dd942e4ff37b6823aa
163002029.junayed <junayednoor@gmail.com>
2022-05-01 11:14:55 +0600

    AOS animation effect added

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Inventory.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Inventory = ({ vehicle }) => {
-  console.log(vehicle);
+    useEffect(() => {
+        AOS.init({
+          duration : 1500
+        });
+      }, []);
   const { img, name, description, supplierName, price, quantity, _id } =
     vehicle;
   return (
     <div>
-      <div className="card h-100" style={{borderRadius:'20px'}}>
+      <div className="card h-100" style={{borderRadius:'20px'}} data-aos="zoom-out">
         <img src={img} className="card-img-top image" style={{height:'400px',objectFit:'cover',borderTopLeftRadius:'20px',borderTopRightRadius:'20px'}} alt="..." />
         <div className="card-body">
           <h2 className="card-title fw-bold">{name}</h2>
