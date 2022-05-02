70f93a6e71eb624b3f48cf0d43e26e83b4927aa8
163002029.junayed <junayednoor@gmail.com>
2022-05-02 06:17:32 +0600

    Inventory restock update, Update inventory

--- src/components/InventoryDetails/InventoryDetails.js
+++ src/components/InventoryDetails/InventoryDetails.js
@@ -1,12 +1,56 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import useInventoryDetail from "../../hooks/useInventoryDetail";
 import "./InventoryDetails.css";
 
 const InventoryDetails = () => {
   const { id } = useParams();
-  const [inventory] = useInventoryDetail(id);
-  console.log(inventory.name);
+  const [inventory, setInventory] = useInventoryDetail(id);
+  const url = `http://localhost:5000/inventory/${id}`;
+  const handleDelivered = (e) => {
+    e.preventDefault();
+    const { quantity, ...rest } = inventory;
+    const updatedInventory = { quantity: parseInt(quantity) - 1, ...rest };
+    
+    fetch(url,{
+        method: 'PUT',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedInventory),
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        toast('Delivered')
+    })
+    setInventory(updatedInventory);
+  };
+  const handleRestock = (e) => {
+    e.preventDefault();
+    const reStock = e.target.stock.value;
+    const { quantity, ...rest } = inventory;
+    if (reStock) {
+        const updatedInventory = {
+            quantity: parseInt(quantity) + Number(reStock),
+            ...rest,
+          };
+          fetch(url,{
+              method:'PUT',
+              headers:{
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(updatedInventory)
+          })
+          .then(response=>response.json())
+          .then(data=>{
+              console.log(data);
+              toast('Your Stock updated')
+          })
+          setInventory(updatedInventory);
+          e.target.reset();
+    }
+  };
   return (
     <div>
       <div
@@ -22,7 +66,7 @@ const InventoryDetails = () => {
         />
         <p>{inventory.description}</p>
         <h3 className="text-center text-primary">Price: ${inventory.price}</h3>
-        <h3 className="text-center text-primary mb-3">
+        <h3 className="text-center text-danger mb-3 fw-bold">
           Total Stock:{inventory.quantity}
         </h3>
         <button
@@ -35,6 +79,7 @@ const InventoryDetails = () => {
             letterSpacing: "2px",
           }}
           className="mx-auto p-2 d-block fw-bold inventory-btn"
+          onClick={handleDelivered}
         >
           Delivered
         </button>
@@ -44,21 +89,32 @@ const InventoryDetails = () => {
         style={{ maxWidth: "800px", borderRadius: "50px" }}
         className="mx-auto mt-5 container shadow-lg"
       >
-          <h1 style={{color:'#22c55e'}} className="text-center">Please Restock your product</h1>
-          <input placeholder="Quantity" type="number" name="quantity" id="" style={{width:'200px',borderRadius:'20px'}} className="d-block mx-auto p-2"/>
+        <h1 style={{ color: "#22c55e" }} className="text-center">
+          Please Restock your product
+        </h1>
+        <form onSubmit={handleRestock}>
+          <input
+            placeholder="Quantity"
+            type="number"
+            name="stock"
+            id=""
+            style={{ width: "200px", borderRadius: "20px" }}
+            className="d-block mx-auto p-2"
+          />
           <button
-          style={{
-            background: "#22c55e",
-            color: "#fff",
-            border: "none",
-            width: "200px",
-            borderRadius: "10px",
-            letterSpacing: "2px",
-          }}
-          className="mx-auto mt-2 p-2 d-block fw-bold inventory-btn"
-        >
-          Restock
-        </button>
+            style={{
+              background: "#22c55e",
+              color: "#fff",
+              border: "none",
+              width: "200px",
+              borderRadius: "10px",
+              letterSpacing: "2px",
+            }}
+            className="mx-auto mt-2 p-2 d-block fw-bold inventory-btn"
+          >
+            Restock
+          </button>
+        </form>
         <div style={{ height: "15px" }}></div>
       </div>
     </div>
--- src/hooks/useInventoryDetail.js
+++ src/hooks/useInventoryDetail.js
@@ -8,6 +8,6 @@ const useInventoryDetail = (id) => {
     .then(res=>res.json())
     .then(data=> setInventory(data))
   }, [id]);
-  return [inventory];
+  return [inventory,setInventory];
 };
 export default useInventoryDetail;
