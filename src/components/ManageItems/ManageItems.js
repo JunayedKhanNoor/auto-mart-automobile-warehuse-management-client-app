680ef29af0ef9ca64cef63eb84abb8ef09deb7a7
163002029.junayed <junayednoor@gmail.com>
2022-05-02 13:11:37 +0600

    Manage inventory page Pagination done

--- src/components/ManageItems/ManageItems.js
+++ src/components/ManageItems/ManageItems.js
@@ -1,20 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Modal, Table } from "react-bootstrap";
 import useVehicles from "../../hooks/useVehicles";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
+import './ManageItems.css';
 
 const ManageItems = () => {
-  let [ID,setID]  = useState();  
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [pageSize,setPageSize] = useState(10);
+  const [inventories, setInventories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/inventoryCount")
+      .then((res) => res.json())
+      .then((data) => {
+        const page = Math.ceil(data.count / pageSize);
+        setPageCount(page);
+        console.log(pageCount);
+      });
+  }, [pageCount,pageSize]);
+  useEffect(()=>{
+      fetch(`http://localhost:5000/inventoriesPage?page=${page}&size=${pageSize}`)
+      .then(res=>res.json())
+      .then(data=>{
+        setInventories(data)
+      })
+  },[page,pageSize])
+  let [ID, setID] = useState("");
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = (id) => {
-      setShow(true);
-      setID(id)
-    };
+    setShow(true);
+    setID(id);
+  };
   const navigate = useNavigate();
   const [vehicles, setVehicles] = useVehicles();
-  let no = 0;
+  let no = (page)*10;
   const number = () => {
     no = no + 1;
     return no;
@@ -22,17 +43,19 @@ const ManageItems = () => {
   const handleModal = () => {
     console.log(ID);
     const url = `http://localhost:5000/inventory/${ID}`;
-      fetch(url, {
-        method: "DELETE",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          const remaining = vehicles.filter((vehicle) => vehicle._id !== ID);
-          setVehicles(remaining);
-        });  
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const remaining = vehicles.filter((vehicle) => vehicle._id !== ID);
+        setVehicles(remaining);
+        const remainingPagination = inventories.filter((vehicle2) => vehicle2._id !== ID);
+        setInventories(remainingPagination);
+      });
     handleClose();
-    return 
+    return;
   };
   return (
     <div className="container bg-white shadow-lg p-5">
@@ -48,14 +71,16 @@ const ManageItems = () => {
           </tr>
         </thead>
         <tbody>
-          {vehicles.map((vehicle) => (
+          {inventories.map((vehicle) => (
             <tr key={vehicle._id}>
               <td>{number()}</td>
               <td>{vehicle.name}</td>
               <td>{vehicle.supplierName}</td>
               <td>{vehicle.quantity}</td>
               <td
-                onClick={()=>{handleShow(vehicle._id)}}
+                onClick={() => {
+                  handleShow(vehicle._id);
+                }}
                 className="ps-3 pt-1 col-2"
                 role="button"
               >
@@ -69,6 +94,14 @@ const ManageItems = () => {
           ))}
         </tbody>
       </Table>
+      <div className="pagination">
+          {[...Array(pageCount).keys()].map(number=>(
+              <button
+              onClick={()=>{setPage(number)}}
+              className={page === number && 'selected'}
+              >{number+1}</button>
+          ))}
+      </div>
       <div className="d-flex justify-content-center my-5">
         <button
           className="btn p-3 manage-btn"
