9f330acbeaf9952a47e19c1e5cbd4e2b04de08ad
163002029.junayed <junayednoor@gmail.com>
2022-05-02 23:34:12 +0600

    Extra one section added in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import Inventories from "../Inventories/Inventories";
-import './Home.css'
+import './Home.css';
+import {SiBmw, SiMercedes,SiFord,SiChevrolet} from 'react-icons/si';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -24,6 +25,20 @@ const Home = () => {
       <Inventories></Inventories>
       <div className="d-flex justify-content-center my-5">
         <button className="btn p-3 manage-btn" onClick={()=>{navigate('/manageItems')}}>Manage Inventories</button>
+      </div>
+      <div style={{backgroundColor:'#fee2e2'}}>
+        <div className="container row py-4  mx-auto">
+        <div className="col-sm-12 col-md-5 detail">
+          <h1><i>Why Choose Us</i></h1>
+          <h1>Our premium clients</h1>
+        </div>
+        <div className="col-sm-12 col-md-7">
+          <SiBmw className="mx-3 company-logo" style={{height:'100px',width:'100px'}}></SiBmw>
+          <SiMercedes className="mx-3 company-logo" style={{height:'100px',width:'100px'}}></SiMercedes>
+          <SiFord className="mx-3 company-logo" style={{height:'100px',width:'100px'}}></SiFord>
+          <SiChevrolet className="mx-3 company-logo" style={{height:'100px',width:'100px'}}></SiChevrolet>
+        </div>
+        </div>
       </div>
     </div>
   );
