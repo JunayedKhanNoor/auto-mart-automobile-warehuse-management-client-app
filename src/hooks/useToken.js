25608def238388c7d9fe02de4e5fd527d3bb5670
163002029.junayed <junayednoor@gmail.com>
2022-05-04 16:14:35 +0600

    Cleaning code

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -2,21 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const useToken = (user) => {
-  /* const [token, setToken] = useState("");
-  console.log(user.email);
-  useEffect(() => {
-    const getToken = async () => {
-      const email = user?.email;
-      if (email) {
-        const { data } = await axios.post("https://morning-ridge-10289.herokuapp.com/login", {
-          email,
-        });
-        setToken(data.accessToken);
-        localStorage.setItem("accessToken", data.accessToken);
-      }
-    };
-    getToken();
-  }, [user]); */
   const [token, setToken] = useState("");
   useEffect(() => {
     const getToken = async () => {
